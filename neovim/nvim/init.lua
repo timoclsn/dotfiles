947bd8e993eca4a7ca343fddc168f99b0c103218
Timo Clasen <goebeltimo@gmail.com>
2024-09-08 21:10:43 +0200

    Disable netrw Banner in Neovim Explore

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -157,6 +157,8 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.g.netrw_banner = 0
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
