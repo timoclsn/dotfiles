d72c4608d63e4d19bac4e63e7e100f11f8d00cdd
Timo Clasen <goebeltimo@gmail.com>
2024-09-09 12:20:51 +0200

    Increase File Tree Width

--- neovim/nvim/lua/kickstart/plugins/neo-tree.lua
+++ neovim/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -2,25 +2,26 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        position = 'right',
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+	},
+	opts = {
+		filesystem = {
+			window = {
+				position = "right",
+				width = 60,
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+	},
 }
