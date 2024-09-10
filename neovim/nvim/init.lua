aeb1fe28d9e57c9c8e2b73908be7649670514127
Timo Clasen <goebeltimo@gmail.com>
2024-09-10 22:36:24 +0200

    Don’t Open Blank File When Entering Neonim

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -212,6 +212,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    if vim.fn.argc() == 0 then
+      vim.cmd 'Ex'
+    end
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
