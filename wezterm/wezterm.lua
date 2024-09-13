afdd76806210a7f34719d838643991c16035fd30
Timo Clasen <goebeltimo@gmail.com>
2024-09-13 20:09:32 +0200

    Update Wezterm Config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,12 +3,12 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Tokyo Night'
-config.font = wezterm.font 'Operator Mono SSm'
+config.font = wezterm.font 'FiraCode Nerd Font Mono'
 config.font_size = 12
 config.line_height = 1.5
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = 'TITLE | RESIZE'
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 config.macos_window_background_blur = 20
 config.window_padding = {
   left = 8,
@@ -16,9 +16,8 @@ config.window_padding = {
   top = 8,
   bottom = 8,
 }
-config.send_composed_key_when_left_alt_is_pressed = true
-config.send_composed_key_when_right_alt_is_pressed = true
 
+config.send_composed_key_when_left_alt_is_pressed = true
 config.keys = {
   { key = '0', mods = 'CTRL', action = wezterm.action.SendString '\u{1b}[48;5u' },
   { key = '1', mods = 'CTRL', action = wezterm.action.SendString '\u{1b}[49;5u' },
