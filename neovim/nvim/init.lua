17b52403ee04624975dea703ef37588e3f19d461
Timo Clasen <goebeltimo@gmail.com>
2024-09-15 14:25:00 +0200

    Fix C-i in non tmux Neovim and add comments

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -115,7 +115,8 @@ vim.keymap.set('v', '<leader>p', [["_dP]], { desc = 'Paste without copying' })
 vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]], { desc = 'Delete without copying' })
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line down' })
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line up' })
-vim.keymap.set('n', '<C-p>', '<C-i>', { desc = 'Go forward in jumplist' })
+vim.keymap.set('n', '<C-p>', '<C-i>', { desc = 'Go forward in jumplist' }) -- Use C-p in tmux nvim because broken C-i mapping
+vim.keymap.set('n', '<C-i>', '<C-i>', { desc = 'Go forward in jumplist' }) -- Fix C-i in non tmux nvim
 
 vim.keymap.set('n', '<leader>o', function()
   vim.lsp.buf.execute_command {
