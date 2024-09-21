fa1af360dabd6ee276c1843112cd509074049450
Timo Clasen <goebeltimo@gmail.com>
2024-09-21 20:11:05 +0200

    Trigger Autoread When Files Changed On Disk

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -71,21 +71,14 @@ vim.opt.scrolloff = 10
 
 vim.g.netrw_banner = 0
 
--- vim.opt.autoread = true
--- -- Triger `autoread` when files changes on disk
--- -- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
--- -- https://vi.stackexchange.com/questions/13692/prevent-focusgained-autocmd-running-in-command-line-editing-mode
--- vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
---   pattern = '*',
---   command = "if mode() !~ '\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif",
--- })
---
--- -- Notification after file change
--- -- https://vi.stackexchange.com/questions/13091/autocmd-event-for-autoread
--- vim.api.nvim_create_autocmd({ 'FileChangedShellPost' }, {
---   pattern = '*',
---   command = "echohl WarningMsg | echo 'File changed on disk. Buffer reloaded.' | echohl None",
--- })
+-- Triger `autoread` when files changes on disk
+vim.opt.autoread = true
+-- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
+-- https://vi.stackexchange.com/questions/13692/prevent-focusgained-autocmd-running-in-command-line-editing-mode
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
+  pattern = '*',
+  command = "if mode() !~ '\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif",
+})
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
