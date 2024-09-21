d2c305ecaa3acc4b5a262448c4cca0852c1de73d
Timo Clasen <goebeltimo@gmail.com>
2024-09-21 21:40:08 +0200

    Change Command to Toggle Diff View

--- neovim/nvim/lua/custom/plugins/init.lua
+++ neovim/nvim/lua/custom/plugins/init.lua
@@ -78,7 +78,13 @@ return {
   {
     'sindrets/diffview.nvim',
     config = function()
-      vim.keymap.set('n', '<leader>hd', vim.cmd.DiffviewOpen, { desc = 'git [d]iff against index' })
+      vim.keymap.set('n', '<leader>hd', function()
+        if next(require('diffview.lib').views) == nil then
+          vim.cmd 'DiffviewOpen'
+        else
+          vim.cmd 'DiffviewClose'
+        end
+      end, { desc = 'git [d]iff against index' })
     end,
   },
 }
