d23e655b729765ef9f476c54086c6661f12c6b34
Timo Clasen <goebeltimo@gmail.com>
2024-09-21 20:01:26 +0200

    Test

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -71,12 +71,19 @@ vim.opt.scrolloff = 10
 
 vim.g.netrw_banner = 0
 
--- auto-reload files when modified externally
--- https://unix.stackexchange.com/a/383044
-vim.o.autoread = true
-vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'CursorHoldI', 'FocusGained' }, {
-  command = "if mode() != 'c' | checktime | endif",
-  pattern = { '*' },
+-- Triger `autoread` when files changes on disk
+-- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
+-- https://vi.stackexchange.com/questions/13692/prevent-focusgained-autocmd-running-in-command-line-editing-mode
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
+  pattern = '*',
+  command = "if mode() !~ '\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif",
+})
+
+-- Notification after file change
+-- https://vi.stackexchange.com/questions/13091/autocmd-event-for-autoread
+vim.api.nvim_create_autocmd({ 'FileChangedShellPost' }, {
+  pattern = '*',
+  command = "echohl WarningMsg | echo 'File changed on disk. Buffer reloaded.' | echohl None",
 })
 
 -- [[ Basic Keymaps ]]
