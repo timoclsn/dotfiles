db3da0f110b38acc2e42cb9d4a0706ad84512407
Timo Clasen <goebeltimo@gmail.com>
2024-09-23 20:07:57 +0200

    Add Oil Plugin

--- neovim/nvim/lua/custom/plugins/init.lua
+++ neovim/nvim/lua/custom/plugins/init.lua
@@ -87,4 +87,24 @@ return {
       end, { desc = 'git [d]iff against index' })
     end,
   },
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        keymaps = {
+          ['<C-h>'] = false,
+          ['<C-l>'] = false,
+          ['<C-k>'] = false,
+          ['<C-j>'] = false,
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      }
+
+      -- Open parent directory in current window
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+  },
 }
