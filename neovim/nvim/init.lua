fe8a4dc26abfdfbe4a8e589957c697f72bb2a372
Timo Clasen <goebeltimo@gmail.com>
2024-09-24 12:11:31 +0200

    Enable CSS Language Server

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -598,6 +598,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
+        cssls = {},
         gopls = {},
         --
         tailwindcss = {
