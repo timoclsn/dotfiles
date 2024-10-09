ee9ce86a7c28f24575e453058f7be5c57815c4e1
Timo Clasen <goebeltimo@gmail.com>
2024-10-09 19:23:07 +0200

    Add Copy Path Keymap

--- neovim/nvim/lua/keymaps.lua
+++ neovim/nvim/lua/keymaps.lua
@@ -39,3 +39,28 @@ vim.keymap.set('n', '<leader>o', function()
     title = '',
   }
 end, { desc = '[O]rganize Imports' })
+
+vim.keymap.set('n', '<leader>dc', function()
+  -- Get the current buffer's full path
+  local full_path = vim.fn.expand '%:p'
+
+  -- Get the project root directory
+  local root_dir = vim.fn.systemlist('git rev-parse --show-toplevel')[1]
+
+  -- If not in a git repo, try to use the current working directory
+  if vim.v.shell_error ~= 0 then
+    root_dir = vim.fn.getcwd()
+  end
+
+  -- Ensure root_dir ends with a separator
+  if root_dir:sub(-1) ~= '/' then
+    root_dir = root_dir .. '/'
+  end
+
+  -- Remove the root directory from the full path to get the relative path
+  local relative_path = full_path:sub(#root_dir + 1)
+
+  -- Copy to clipboard
+  vim.fn.setreg('+', relative_path)
+  print('Copied path to clipboard: ' .. relative_path)
+end, { noremap = true, silent = true, desc = '[D]ocument [C]opy full path' })
