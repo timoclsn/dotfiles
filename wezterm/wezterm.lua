a4772c7d43710e49015c1ec869b012095fd73c4b
Timo Clasen <goebeltimo@gmail.com>
2024-10-19 22:26:56 +0200

    Update terminal window decoration

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,16 +6,17 @@ config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
 config.font_size = 12
 config.line_height = 1.5
-config.window_decorations = 'TITLE | RESIZE'
+config.window_decorations = 'INTEGRATED_BUTTONS | RESIZE'
+config.adjust_window_size_when_changing_font_size = false
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 20
 config.window_padding = {
-  left = 8,
-  right = 8,
-  top = 8,
-  bottom = 8,
+  left = '1cell',
+  right = '1cell',
+  top = '1.2cell',
+  bottom = '0',
 }
 config.underline_position = -4
 
