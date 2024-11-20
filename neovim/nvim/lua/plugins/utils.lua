908fb755b2493d7a6f40a1b48e5561f9bfe1e88b
Timo Clasen <goebeltimo@gmail.com>
2024-11-20 16:20:54 +0100

    Use more mini plugins and update all plugins

--- neovim/nvim/lua/plugins/utils.lua
+++ neovim/nvim/lua/plugins/utils.lua
@@ -63,19 +63,6 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = { signs = false },
   },
-  {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    -- Optional dependency
-    dependencies = { 'hrsh7th/nvim-cmp' },
-    config = function()
-      require('nvim-autopairs').setup {}
-      -- If you want to automatically add `(` after selecting a function or method
-      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-      local cmp = require 'cmp'
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-    end,
-  },
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
