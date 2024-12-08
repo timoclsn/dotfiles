9f906a3e85c58cee55e4b9732ea891817b73bcd1
Timo Clasen <goebeltimo@gmail.com>
2024-12-08 20:38:27 +0100

    Decrease scrolloff

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -60,7 +60,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 8
 
 vim.g.netrw_banner = 0
 
