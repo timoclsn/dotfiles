0425b2f5ac6d407b04f0b236043a36c2a459b53a
Timo Clasen <goebeltimo@gmail.com>
2024-12-10 20:52:38 +0100

    Enable diagnostics in statusline

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -35,16 +35,9 @@ return {
         footer = 'Happy coding!',
       }
 
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
         return '%2l(%L):%-2v'
@@ -52,11 +45,6 @@ return {
 
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_lsp = function()
-        return ''
-      end
-
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_diagnostics = function()
         return ''
       end
 
