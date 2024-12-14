8fcb4b21e30ec513e04cebc59fe0cd6711597eb6
Timo Clasen <goebeltimo@gmail.com>
2024-12-14 17:13:58 +0100

    Optimize path_display

--- nvim/lua/telescope/path_display.lua
+++ nvim/lua/telescope/path_display.lua
@@ -1,22 +1,37 @@
-local path_formatter = require 'utils.path_formatter'
+-- Cache frequently used functions
+local nvim_create_autocmd = vim.api.nvim_create_autocmd
+local nvim_buf_call = vim.api.nvim_buf_call
+local nvim_set_hl = vim.api.nvim_set_hl
+local format_path = require('utils.path_formatter').format_path
+local match_add = vim.fn.matchadd
 
-local custom_path_display = function(_, path)
-  local formatted = path_formatter.format_path(path)
+-- Pre-compile the format string
+local PATH_FORMAT = '%s\t\t%s'
 
-  if formatted.relative_path == '.' or formatted.relative_path == '' then
+-- Define the display function directly
+local function custom_path_display(_, path)
+  local formatted = format_path(path)
+  local rel_path = formatted.relative_path
+
+  -- Use direct comparison instead of multiple conditions
+  if rel_path == '.' or rel_path == '' then
     return formatted.filename
   end
-  return string.format('%s\t\t%s', formatted.filename, formatted.relative_path)
+
+  -- Use pre-compiled format string
+  return PATH_FORMAT:format(formatted.filename, rel_path)
 end
 
-vim.api.nvim_create_autocmd('FileType', {
+-- Single autocmd setup with optimized callback
+nvim_create_autocmd('FileType', {
   pattern = 'TelescopeResults',
   callback = function(ctx)
-    vim.api.nvim_buf_call(ctx.buf, function()
-      vim.fn.matchadd('TelescopeParent', '\t\t.*$')
-      vim.api.nvim_set_hl(0, 'TelescopeParent', { link = 'Comment' })
+    nvim_buf_call(ctx.buf, function()
+      match_add('TelescopeParent', '\t\t.*$')
+      nvim_set_hl(0, 'TelescopeParent', { link = 'Comment' })
     end)
   end,
 })
 
+-- Return the function directly
 return custom_path_display
