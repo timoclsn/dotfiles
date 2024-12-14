79c11c4ef1508d784c6c72688b5a7797d72493c6
Timo Clasen <goebeltimo@gmail.com>
2024-12-14 22:14:32 +0100

    Update telescope results styling

--- nvim/lua/telescope/path_display.lua
+++ nvim/lua/telescope/path_display.lua
@@ -22,8 +22,7 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = 'TelescopeResults',
   callback = function(ctx)
     vim.api.nvim_buf_call(ctx.buf, function()
-      vim.fn.matchadd('TelescopeParent', '\t\t.*$')
-      vim.api.nvim_set_hl(0, 'TelescopeParent', { link = 'Comment' })
+      vim.fn.matchadd('TelescopeResultsComment', '\t\t.*$')
     end)
   end,
 })
