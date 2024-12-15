fe2083c6149db01bdad97de1716373b18af884c1
Timo Clasen <goebeltimo@gmail.com>
2024-12-15 14:07:50 +0100

    Add pretty ts errors plugin

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -142,4 +142,13 @@ return {
       },
     },
   },
+  {
+    'OlegGulevskyy/better-ts-errors.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    config = {
+      keymaps = {
+        toggle = '<leader>K',
+      },
+    },
+  },
 }
