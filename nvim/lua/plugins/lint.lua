29895aab7240ac10dddd6e905b3ebc4383121b04
Timo Clasen <goebeltimo@gmail.com>
2024-12-21 22:04:14 +0100

    Disable markdown linting

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -5,7 +5,9 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = {
+        --   'markdownlint',
+        -- },
         javascript = {
           'eslint_d',
         },
