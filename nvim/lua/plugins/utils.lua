8dc538f13249056612116d2bbdf92ae328c0c3f9
Timo Clasen <goebeltimo@gmail.com>
2025-01-02 21:19:46 +0100

    Add ghostty config file syntax highlighting

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -164,4 +164,14 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
     opts = {},
   },
+  -- Provides syntax highlighting for the ghostty config file
+  -- No additional dependencies required, Zilchmasta was kind enough to let me
+  -- know about this in discord
+  -- https://discord.com/channels/1005603569187160125/1300462095946485790/1300534513788653630
+
+  {
+    'ghostty',
+    dir = '/Applications/Ghostty.app/Contents/Resources/vim/vimfiles/',
+    lazy = false,
+  },
 }
