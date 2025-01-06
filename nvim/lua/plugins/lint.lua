aae22a6b95a21618d64d665a0ee72c97c36a71ba
Timo Clasen <goebeltimo@gmail.com>
2025-01-06 22:35:06 +0100

    Update lint config

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -4,6 +4,7 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
+      vim.env.ESLINT_D_PPID = vim.fn.getpid()
       lint.linters_by_ft = {
         -- markdown = {
         --   'markdownlint',
