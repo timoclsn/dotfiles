28d430ea06f417f01cc02833b96e57a1401b88c9
Timo Clasen <goebeltimo@gmail.com>
2025-01-20 10:01:24 +0100

    Open smart open split grep results at the correct line and column if available

--- nvim/lua/telescope/smart_opener.lua
+++ nvim/lua/telescope/smart_opener.lua
@@ -3,6 +3,18 @@ local M = {}
 local action_state = require 'telescope.actions.state'
 local actions = require 'telescope.actions'
 
+local function open_file_with_position(selection, cmd)
+  local filename = selection.filename or selection.value
+  if selection.lnum then -- If we have line number info (from live grep)
+    -- Open file and move to the correct position
+    vim.cmd(cmd .. ' ' .. filename)
+    vim.api.nvim_win_set_cursor(0, { selection.lnum, selection.col or 0 })
+  else
+    -- Just open the file
+    vim.cmd(cmd .. ' ' .. filename)
+  end
+end
+
 M.smart_open = function(prompt_bufnr)
   local picker = action_state.get_current_picker(prompt_bufnr)
   local multi_selections = picker:get_multi_selection()
@@ -13,9 +25,9 @@ M.smart_open = function(prompt_bufnr)
     local current_selection = action_state.get_selected_entry()
     for _, selection in ipairs(multi_selections) do
       if selection == current_selection then
-        vim.cmd('edit ' .. selection.value)
+        open_file_with_position(selection, 'edit')
       else
-        vim.cmd('badd ' .. selection.value)
+        vim.cmd('badd ' .. (selection.filename or selection.value))
       end
     end
   else
@@ -32,17 +44,17 @@ M.smart_open_split = function(prompt_bufnr)
     -- Multiple files selected, open the highlighted one on the left and others in vertical splits to the right
     actions.close(prompt_bufnr)
     local current_selection = action_state.get_selected_entry()
-    vim.cmd('edit ' .. (current_selection.filename or current_selection.value))
+    open_file_with_position(current_selection, 'edit')
     for _, selection in ipairs(multi_selections) do
       if selection ~= current_selection then
-        vim.cmd('vsplit ' .. (selection.filename or selection.value))
+        open_file_with_position(selection, 'vsplit')
       end
     end
   else
     -- Single file selected (or no selection), open in a vertical split
     actions.close(prompt_bufnr)
     local current_selection = action_state.get_selected_entry()
-    vim.cmd('vsplit ' .. (current_selection.filename or current_selection.value))
+    open_file_with_position(current_selection, 'vsplit')
   end
 end
 
