944c191114c46a71c9ae8fd5e79a8e54404d14b5
Timo Clasen <goebeltimo@gmail.com>
2025-02-01 21:51:34 +0100

    Re-add whichkey delay

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -6,7 +6,6 @@ return {
     'folke/which-key.nvim',
     event = 'VimEnter',
     opts = {
-      delay = 0,
       icons = {
         -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
