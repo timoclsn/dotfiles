aed0f48595e9ecf6a01bab592d685a0baece98c9
Timo Clasen <goebeltimo@gmail.com>
2025-03-14 22:11:19 +0100

    Add jsx treesitter objects

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -182,4 +182,13 @@ return {
     },
     opts = {},
   },
+  {
+    'mawkler/jsx-element.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    ft = { 'typescriptreact', 'javascriptreact', 'javascript' },
+    opts = {},
+  },
 }
