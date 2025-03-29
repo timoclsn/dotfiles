e84dd8aa61bb4d00976049b3b32ae0ccefb0d31e
Timo Clasen <goebeltimo@gmail.com>
2025-03-29 20:00:02 +0100

    Update fold config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -121,4 +121,16 @@ vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.foldtext = ''
 vim.o.foldmethod = 'expr'
-vim.o.foldexpr = 'v:lua.vim.lsp.foldexpr()'
+-- Default to treesitter folding
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+-- Prefer LSP folding if client supports it
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client:supports_method 'textDocument/foldingRange' then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    end
+  end,
+})
+vim.api.nvim_create_autocmd('LspDetach', { command = 'setl foldexpr<' })
