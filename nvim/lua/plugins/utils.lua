773f4bc50269bbcb9ea4359f2b6c6572e9a7d200
Timo Clasen <goebeltimo@gmail.com>
2025-04-03 16:28:27 +0200

    Consistantly use nvim-tree/nvim-web-devicons

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -137,7 +137,7 @@ return {
   },
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
     opts = {},
   },
   -- Provides syntax highlighting for the ghostty config file
