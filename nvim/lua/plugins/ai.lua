04993b84e8ebd72cef6e99f0dbb684fc9fa0ef0d
Timo Clasen <goebeltimo@gmail.com>
2025-04-05 20:10:07 +0200

    Remove avante image plugin

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -13,6 +13,7 @@ return {
         auto_trigger = true,
         keymap = {
           accept = '<Tab>',
+
           prev = '<C-k>',
           next = '<C-j>',
           dismiss = '<C-c>',
@@ -32,23 +33,6 @@ return {
       --- The below dependencies are optional,
       'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
       'zbirenbaum/copilot.lua', -- for providers='copilot'
-      {
-        -- support for image pasting
-        'HakonHarnes/img-clip.nvim',
-        event = 'VeryLazy',
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
       {
         -- Make sure to set this up properly if you have lazy=true
         'MeanderingProgrammer/render-markdown.nvim',
