fffb65601315572a879bf537fe458a8f5c59b979
Timo Clasen <goebeltimo@gmail.com>
2025-04-07 21:29:04 +0200

    Update which key delay handling

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -6,6 +6,7 @@ return {
     'folke/which-key.nvim',
     event = 'VimEnter',
     opts = {
+      delay = 500,
       icons = {
         -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
