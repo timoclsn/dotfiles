71c1d3f14688b5623853cd501290cf8a76c701c7
Timo Clasen <goebeltimo@gmail.com>
2025-04-16 08:38:19 +0200

    Remove custom right click menu

---
+++ nvim/lua/utils/menu.lua
@@ -0,0 +1,38 @@
+vim.cmd [[
+  -- aunmenu PopUp
+  anoremenu PopUp.Inspect     <cmd>Inspect<CR>
+  amenu PopUp.-1-             <NOP>
+  anoremenu PopUp.LspDefinition  <cmd>lua vim.lsp.buf.definition()<CR>
+  anoremenu PopUp.LspReferences  <cmd>Telescope lsp_references<CR>
+  nnoremenu PopUp.Back        <C-t>
+  amenu PopUp.-2-             <NOP>
+  amenu     PopUp.Hyperlink     gX
+]]
+
+local group = vim.api.nvim_create_augroup('nvim_popupmenu', { clear = true })
+
+-- vim.api.nvim_create_autocmd('MenuPopup', {
+--   pattern = '*',
+--   group = group,
+--   desc = 'Custom PopUp Setup',
+--   callback = function()
+--     -- vim.cmd [[
+--     --   amenu disable PopUp.LspDefinition
+--     --   amenu disable PopUp.LspReferences
+--     --   amenu disable PopUp.Hyperlink
+--     -- ]]
+--
+--     if vim.lsp.get_clients({ bufnr = 0 })[1] then
+--       -- vim.cmd [[
+--       --   amenu enable PopUp.LspDefinition
+--       --   amenu enable PopUp.LspReferences
+--       -- ]]
+--     end
+--
+--     local url = vim.fn.expand '<cfile>'
+--
+--     if url and vim.startswith(url, 'http') then
+--       -- vim.cmd [[amenu enable PopUp.Hyperlink]]
+--     end
+--   end,
+-- })
