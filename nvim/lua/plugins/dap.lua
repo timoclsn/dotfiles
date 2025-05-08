95a5112a9a42981741f4e663e0b57b1874837b58
Timo Clasen <goebeltimo@gmail.com>
2025-05-08 21:29:12 +0200

    Add dap setup

---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,110 @@
+return {
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      { 'rcarriga/nvim-dap-ui', dependencies = { 'nvim-neotest/nvim-nio' } },
+      'mxsdev/nvim-dap-vscode-js',
+      -- build debugger from source
+      {
+        'microsoft/vscode-js-debug',
+        version = '1.x',
+        build = 'npm i && npm run compile vsDebugServerBundle && mv dist out',
+      },
+    },
+    keys = {
+      -- Your key mappings are fine
+      {
+        '<leader>db',
+        function()
+          require('dap').toggle_breakpoint()
+        end,
+      },
+      {
+        '<leader>dc',
+        function()
+          require('dap').continue()
+        end,
+      },
+      {
+        "<C-'>",
+        function()
+          require('dap').step_over()
+        end,
+      },
+      {
+        '<C-;>',
+        function()
+          require('dap').step_into()
+        end,
+      },
+      {
+        '<C-:>',
+        function()
+          require('dap').step_out()
+        end,
+      },
+    },
+    config = function()
+      -- First, set up the adapter explicitly
+      local dap = require 'dap'
+
+      -- This is the key fix - explicitly define the adapter with port
+      dap.adapters['pwa-chrome'] = {
+        type = 'server',
+        host = 'localhost',
+        port = 9222, -- Chrome debugging port
+      }
+
+      -- Now set up dap-vscode-js
+      require('dap-vscode-js').setup {
+        debugger_path = vim.fn.stdpath 'data' .. '/lazy/vscode-js-debug',
+        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+      }
+
+      for _, language in ipairs { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact' } do
+        require('dap').configurations[language] = {
+          -- Your configurations are fine
+          {
+            type = 'pwa-node',
+            request = 'attach',
+            processId = require('dap.utils').pick_process,
+            name = 'Attach debugger to existing `node --inspect` process',
+            sourceMaps = true,
+            resolveSourceMapLocations = {
+              '${workspaceFolder}/**',
+              '!**/node_modules/**',
+            },
+            cwd = '${workspaceFolder}/src',
+            skipFiles = { '${workspaceFolder}/node_modules/**/*.js' },
+          },
+          {
+            type = 'pwa-chrome',
+            name = 'Launch Chrome to debug client',
+            request = 'launch',
+            url = 'http://localhost:3000',
+            sourceMaps = true,
+            protocol = 'inspector',
+            port = 9222,
+            webRoot = '${workspaceFolder}/src',
+            skipFiles = { '**/node_modules/**/*', '**/@vite/*', '**/src/client/*', '**/src/*' },
+          },
+          language == 'javascript' and {
+            type = 'pwa-node',
+            request = 'launch',
+            name = 'Launch file in new node process',
+            program = '${file}',
+            cwd = '${workspaceFolder}',
+          } or nil,
+        }
+      end
+
+      require('dapui').setup()
+      local dapui = require 'dapui'
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open { reset = true }
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+      dap.listeners.before.event_exited['dapui_config'] = dapui.close
+    end,
+  },
+}
