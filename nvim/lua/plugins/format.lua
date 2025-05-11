9e83805552c7352de05a8fa1ea822e165bc1512d
Timo Clasen <goebeltimo@gmail.com>
2025-05-11 16:22:18 +0200

    Update format config formatting

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -49,7 +49,11 @@ return {
       return {
         notify_on_error = false,
         format_on_save = function(bufnr)
-          local disable_filetypes = { c = true, cpp = true }
+          local disable_filetypes = {
+            c = true,
+            cpp = true,
+          }
+
           if disable_filetypes[vim.bo[bufnr].filetype] then
             return nil
           else
