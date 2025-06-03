2f5d0b6f716594ecf9442e25d72c41397972f2ef
Timo Clasen <goebeltimo@gmail.com>
2025-06-03 09:08:29 +0200

    Remove copilot score offset

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -93,7 +93,6 @@ return {
           copilot = {
             name = 'copilot',
             module = 'blink-copilot',
-            score_offset = 100,
             async = true,
           },
           avante = {
