b673ce0d7b89384faf6ba1b02971a11d2d404ffb
Timo Clasen <goebeltimo@gmail.com>
2025-06-12 22:26:38 +0200

    feat(nvim): add vague.nvim colorscheme plugin and update lazy-lock.json

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -33,6 +33,10 @@ return {
     priority = 1000,
   },
   {
+    'vague2k/vague.nvim',
+    priority = 1000,
+  },
+  {
     'zenbones-theme/zenbones.nvim',
     dependencies = 'rktjmp/lush.nvim',
     priority = 1000,
