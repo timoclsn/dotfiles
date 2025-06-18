490b72501384ead6ea78fa02056e8a3b2077f231
Timo Clasen <goebeltimo@gmail.com>
2025-06-18 21:54:27 +0200

    refactor(nvim): simplify telescope path display function

--- nvim/lua/telescope/path_display.lua
+++ nvim/lua/telescope/path_display.lua
@@ -1,23 +1,18 @@
 local format_path = require 'utils.path_formatter'
 
--- Pre-compile the format string
 local PATH_FORMAT = '%s\t\t%s'
 
--- Define the display function directly
 local function custom_path_display(_, path)
   local formatted_path = format_path(path)
   local relative_path = formatted_path.relative_path
 
-  -- Use direct comparison instead of multiple conditions
   if relative_path == '.' or relative_path == '' then
     return formatted_path.filename
   end
 
-  -- Use pre-compiled format string
   return PATH_FORMAT:format(formatted_path.filename, relative_path)
 end
 
--- Single autocmd setup with optimized callback
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'TelescopeResults',
   callback = function(ctx)
