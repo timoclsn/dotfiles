57c92430c6c8f7fe30ae0ccade9907c57ffb60ec
Timo Clasen <goebeltimo@gmail.com>
2025-06-18 21:01:08 +0200

    refactor(nvim): simplify ai and files plugin configurations

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -22,40 +22,20 @@ return {
       'stevearc/dressing.nvim',
       'nvim-lua/plenary.nvim',
       'MunifTanjim/nui.nvim',
-      --- The below dependencies are optional,
-      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
       'zbirenbaum/copilot.lua', -- for providers='copilot'
-      {
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { 'markdown', 'Avante' },
-        },
-        ft = { 'markdown', 'Avante' },
-      },
     },
     opts = {
       provider = 'copilot',
       providers = {
         copilot = {
-          -- model = 'claude-3.5-sonnet',
-          model = 'claude-3.7-sonnet',
-          -- model = 'claude-3.7-sonnet-thought',
-          -- tmperature = 1,
-          -- max_tokens = 20000,
+          model = 'claude-3.5-sonnet',
         },
       },
       hints = {
         enabled = false,
       },
-      behaviour = {
-        use_cwd_as_project_root = true,
-        auto_apply_diff_after_generation = true,
-      },
       windows = {
         position = 'left',
-        ask = {
-          start_insert = false,
-        },
       },
     },
   },
