94d47a4202affbf5598a6413734a907b5484478a
Timo Clasen <goebeltimo@gmail.com>
2025-08-28 22:26:20 +0200

    chore(nvim): update nvim plugins

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
   {
-    'echasnovski/mini.nvim',
+    'nvim-mini/mini.nvim',
     depedencies = {
       'nvim-treesitter/nvim-treesitter',
       'nvim-treesitter/nvim-treesitter-textobjects',
