3539ed204c7be97542257d0d0bad6b08ea6e96eb
Timo Clasen <goebeltimo@gmail.com>
2025-12-15 14:43:48 +0100

    chore(nvim): remove fff.nvim plugin and update mason-lspconfig.nvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -133,20 +133,4 @@ return {
       end, { desc = 'Open Buffers' })
     end,
   },
-  {
-    'dmtrKovalenko/fff.nvim',
-    build = function()
-      require('fff.download').download_or_build_binary()
-    end,
-    lazy = false,
-    keys = {
-      {
-        'ff', -- try it if you didn't it is a banger keybinding for a picker
-        function()
-          require('fff').find_files()
-        end,
-        desc = 'FFFind files',
-      },
-    },
-  },
 }
