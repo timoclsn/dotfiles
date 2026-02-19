dbb45559970a3310fc4d73a53147398156afbf24
Timo Clasen <goebeltimo@gmail.com>
2026-02-19 10:59:05 +0100

    fix(nvim): add git base cycling to gitsigns

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,3 +1,14 @@
+local base_modes = { 'index', 'HEAD', 'parent' }
+local current_base_idx = 1
+
+local get_parent_base = function()
+  local base = vim.fn.system('git merge-base HEAD origin/HEAD 2>/dev/null'):gsub('%s+', '')
+  if vim.v.shell_error == 0 and base ~= '' then
+    return base
+  end
+  return nil
+end
+
 return {
   {
     'lewis6991/gitsigns.nvim',
@@ -19,6 +30,26 @@ return {
         vim.keymap.set('n', '<leader>gr', gitsigns.reset_hunk, { desc = '[g]it [r]eset hunk' })
         vim.keymap.set('n', '<leader>gS', gitsigns.stage_buffer, { desc = '[g]it [S]tage buffer' })
         vim.keymap.set('n', '<leader>gR', gitsigns.reset_buffer, { desc = '[g]it [R]eset buffer' })
+        vim.keymap.set('n', '<leader>gb', function()
+          current_base_idx = current_base_idx % #base_modes + 1
+          local mode = base_modes[current_base_idx]
+          if mode == 'index' then
+            gitsigns.reset_base(true)
+            vim.notify('Gitsigns base: index (unstaged changes)')
+          elseif mode == 'HEAD' then
+            gitsigns.change_base('HEAD', true)
+            vim.notify('Gitsigns base: HEAD (uncommitted changes)')
+          else
+            local base = get_parent_base()
+            if base then
+              gitsigns.change_base(base, true)
+              vim.notify('Gitsigns base: parent branch (' .. base:sub(1, 7) .. ')')
+            else
+              gitsigns.reset_base(true)
+              vim.notify('Gitsigns base: parent branch not found, using index')
+            end
+          end
+        end, { desc = '[g]it cycle [b]ase' })
         vim.keymap.set('n', ']h', function()
           if vim.wo.diff then
             vim.cmd.normal { ']h', bang = true }
